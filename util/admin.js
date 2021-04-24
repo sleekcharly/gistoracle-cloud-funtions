67803fbc049e31a99d7a5fac97d4568feb3f4fd2
sleekcharly <charlyboy4real@gmail.com>
2021-04-24 13:04:23 +0100

    updated environment variable for GCLOUD_PROJECT

--- util/admin.js
+++ util/admin.js
@@ -4,29 +4,33 @@
 // to get access to the database we would need the admin SDK
 const admin = require("firebase-admin");
 
+// set environment project id
+const project = process.env.GCLOUD_PROJECT;
+
 /*Your Firebase service account can be used to authenticate 
 multiple Firebase features, such as Database, Storage and
  Auth, programmatically via the unified Admin SDK */
 var serviceAccount =
-  process.env.NODE_ENV === "production"
+  project === "gistoracle-28360"
     ? require("../gistoracle-28360-firebase-adminsdk-ewftv-9107d0cc69")
     : require("../gistoracle-dev-firebase-adminsdk-4qg56-ce04c6150b.json");
-
-// set environment
-const environment = process.env.NODE_ENV;
 
 // intializing the application to use the admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket:
-    environment === "production"
+    project === "gistoracle-28360"
       ? process.env.FIREBASE_STORAGE_BUCKET
       : process.env.FIREBASE_DEVELOPMENT_STORAGE_BUCKET,
   databaseURL:
-    environment === "production"
+    project === "gistoracle-28360"
       ? process.env.FIREBASE_DATABASE_URL
       : process.env.FIREBASE_DEVELOPMENT_DATABASE_URL,
 });
+
+console.log(project);
+
+// console.log("environment: " + process.env.NODE_ENV);
 
 // instantiate the firebase store
 const db = admin.firestore();
--- util/config.js
+++ util/config.js
@@ -3,35 +3,35 @@
 // paste the firebase config file for initialization and authentication.
 module.exports = {
   apiKey:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_API_KEY
       : process.env.FIREBASE_DEVELOPMENT_API_KEY,
   authDomain:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_AUTH_DOMAIN
       : process.env.FIREBASE_DEVELOPMENT_AUTH_DOMAIN,
   databaseURL:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_DATABASE_URL
       : process.env.FIREBASE_DEVELOPMENT_DATABASE_URL,
   projectId:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_PROJECT_ID
       : process.env.FIREBASE_DEVELOPMENT_PROJECT_ID,
   storageBucket:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_STORAGE_BUCKET
       : process.env.FIREBASE_DEVELOPMENT_STORAGE_BUCKET,
   messagingSenderId:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_MESSAGING_SENDER_ID
       : process.env.FIREBASE_DEVELOPMENT_MESSAGING_SENDER_ID,
   appId:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_APP_ID
       : process.env.FIREBASE_DEVELOPMENT_APP_ID,
   measurementId:
-    process.env.NODE_ENV === "production"
+    process.env.GCLOUD_PROJECT === "gistoracle-28360"
       ? process.env.FIREBASE_MEASUREMENT_ID
       : process.env.FIREBASE_MEASUREMENT_ID,
 };
